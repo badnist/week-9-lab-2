8caf8f35b9db44a83a460959d1c4b611a3b4f023
badnist <ryan.marzban@outlook.com>
2024-07-26 20:12:24 +0200

    updated

---
+++ lab-vue-c-basics/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import NavBar from "./components/NavBar.vue";
+import MainComp from "./components/MainComponent.vue";
+import FooterComponent from "./components/FooterComponent.vue";
+
+</script>
+
+
+<template>
+  <NavBar />
+  <MainComponent />
+  <FooterComponent />
+</template>
---
+++ lab-vue-c-basics/src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <nav>
+        <ul>
+            <li>
+                <h2>Iteración 1</h2>
+                <p class="iteration-explanation">
+                    Create a basic template structure within a new project
+                </p>
+            </li>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">Contact</a></li>
+        </ul>
+    </nav>
+</template>
+
+<style scoped>
+ul {
+    display: flex;
+    justify-content: space-between;
+    list-style: none;
+    padding: 0;
+    align-items: center;
+}
+</style>
